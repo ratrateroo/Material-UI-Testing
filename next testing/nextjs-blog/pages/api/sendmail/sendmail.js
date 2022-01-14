fbf7a7de9ea1f47e500c2395db3a3c82c6da9bf1
Mark <ratrateroo@gmail.com>
2022-01-14 15:40:48 +0800

    started testing radio buttons

--- next testing/nextjs-blog/pages/api/sendmail/sendmail.js
+++ next testing/nextjs-blog/pages/api/sendmail/sendmail.js
@@ -56,45 +56,45 @@ const sendmail = async (req, res) => {
 			sendEmail();
 		});
 
-	// const pathToAttachment = path.join(
-	// 	serverRuntimeConfig.PROJECT_ROOT,
-	// 	'./test.pdf'
-	// );
-	//console.log(pathToAttachment);
+	const pathToAttachment = path.join(
+		serverRuntimeConfig.PROJECT_ROOT,
+		'./test.pdf'
+	);
+	console.log(pathToAttachment);
 
-	// const attachment = await fs
-	// 	.readFileSync(pathToAttachment)
-	// 	.toString('base64');
-	// console.log(attachment);
-	// const message = {
-	// 	to: 'ratrateroox@gmail.com',
-	// 	from: 'ratrateroox@gmail.com',
-	// 	subject: 'Sendgrid Email',
-	// 	text: 'Test message.',
-	// 	html: req.body.htmlContent,
-	// 	attachments: [
-	// 		{
-	// 			content: attachment,
-	// 			filename: 'test.pdf',
-	// 			type: 'application/pdf',
-	// 			disposition: 'attachment',
-	// 		},
-	// 	],
-	// };
-	// const sendEmail = async () => {
-	// 	await sendGridMail
-	// 		.send(message)
-	// 		.then((response) => {
-	// 			console.log(response[0].statusCode);
-	// 			console.log(response[0].headers);
-	// 		})
-	// 		.then(() => {
-	// 			console.log('Message Sent!');
-	// 		})
-	// 		.catch((error) => {
-	// 			console.log(error);
-	// 		});
-	// };
+	const attachment = await fs
+		.readFileSync(pathToAttachment)
+		.toString('base64');
+	console.log(attachment);
+	const message = {
+		to: 'ratrateroox@gmail.com',
+		from: 'ratrateroox@gmail.com',
+		subject: 'Sendgrid Email',
+		text: 'Test message.',
+		html: req.body.htmlContent,
+		attachments: [
+			{
+				content: attachment,
+				filename: 'test.pdf',
+				type: 'application/pdf',
+				disposition: 'attachment',
+			},
+		],
+	};
+	const sendEmail = async () => {
+		await sendGridMail
+			.send(message)
+			.then((response) => {
+				console.log(response[0].statusCode);
+				console.log(response[0].headers);
+			})
+			.then(() => {
+				console.log('Message Sent!');
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 	//sendEmail();
 
 	res.status(200).send({
